2dd3ce03fec88dcbe9253a3075262f1072aa057c
iamBharatManral <justbharatmanral@gmail.com>
2023-09-13 09:05:21 +0530

    ui: add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Expense Tracker
+      <header>
+          <h1>Expense Tracker</h1>
+      </header>
     </div>
   );
 }
