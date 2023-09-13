5d40dcaab7df27899455a6bc1a01f36a18db1ef1
iamBharatManral <justbharatmanral@gmail.com>
2023-09-13 10:03:24 +0530

    ui: change style for ExpenseItem component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import "./ExpenseItem.css"
 
-function ExpenseItem(props) {
+function ExpenseItem({date, title, amount}) {
+    const month = date.toLocaleString('en-US', {month: 'long'})
+    const day = date.toLocaleString('en-US', {day: '2-digit'})
+    const year = date.getFullYear()
+
     return (
         <div className={"expenseItem"}>
-            <div className={"date"}>Date</div>
-            <div className={"title"}>car insurance</div>
-            <div className={"amount"}>Amount</div>
+            <div className={"date"}>
+                <div className={"month"}>{month}</div>
+                <div className={"year"}>{year}</div>
+                <div className={'day'}>{day}</div>
+            </div>
+            <div className={"title"}>{title}</div>
+            <div className={"amount"}>${amount}</div>
         </div>
     );
 }
